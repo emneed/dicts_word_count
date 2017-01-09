082b75e74ead4ec42c2bf24f81b1b023624cde53
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 12:30:24 -0800

    Altered get_word_count to alphabetize output

--- wordcount.py
+++ wordcount.py
@@ -28,17 +28,13 @@ def get_word_count(file_name):
         word = no_punc.lower()
         word_count[word] = word_count.get(word, 0) + 1
 
-    # iterates through word_count to print out the key-value pairs
-    # uses iteritems() for longer dictionaries
-    if len(word_count) > 100:
-        for word, count in word_count.iteritems():
-            print "%s %d" % (word, count)
-    else:
-        for word, count in word_count.items():
-            print "%s %d" % (word, count)
+    # sorts the words alphbetically then prints them with their values
+    words = sorted(word_count)
+    for word in words:
+        print "%s %d" % (word, word_count[word])
 
 
-#get_word_count(input_file)
+get_word_count(input_file)
 
 
 def get_word_count_counter(file_name):
@@ -66,4 +62,4 @@ def get_word_count_counter(file_name):
     for word, count in counter.most_common():
         print "%s %d" % (word, count)
 
-get_word_count_counter(input_file)
+#get_word_count_counter(input_file)
