40b5d0b8813959967441a44e5f40b093baca3d14
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 12:25:52 -0800

    Added function to use collections.Counter

--- wordcount.py
+++ wordcount.py
@@ -1,6 +1,7 @@
 # put your code here.
 import string
 import sys
+import collections
 
 input_file = sys.argv[1]
 
@@ -28,6 +29,7 @@ def get_word_count(file_name):
         word_count[word] = word_count.get(word, 0) + 1
 
     # iterates through word_count to print out the key-value pairs
+    # uses iteritems() for longer dictionaries
     if len(word_count) > 100:
         for word, count in word_count.iteritems():
             print "%s %d" % (word, count)
@@ -36,4 +38,32 @@ def get_word_count(file_name):
             print "%s %d" % (word, count)
 
 
-get_word_count(input_file)
+#get_word_count(input_file)
+
+
+def get_word_count_counter(file_name):
+
+    data_file = open(file_name)
+
+    #word_count = {}
+    tokens = []
+
+    # splits file into lines, then splits each line on a space
+    for line in data_file:
+        line = line.rstrip()
+
+        # makes one large list of words instead of working line by line
+        tokens = tokens + line.split(" ")
+
+    # goes through each token and strips punctuation and makes lowercase
+    # to eliminate doubles, and does dictionary counting
+    exclude = set(string.punctuation)
+    for index, word in enumerate(tokens):
+        no_punc = "".join(char for char in word if char not in exclude)
+        tokens[index] = no_punc.lower()
+
+    counter = collections.Counter(tokens)
+    for word, count in counter.most_common():
+        print "%s %d" % (word, count)
+
+get_word_count_counter(input_file)
